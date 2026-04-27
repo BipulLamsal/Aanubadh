778e856fa04eae2bd2f831a346037a2fd1a97a60
Bipul Lamsal <bipullamsal@gmail.com>
2026-04-27 23:58:13 +0545

    still processing the idea

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use docx_rs::{self, DocumentChild, Docx, ParagraphChild, RunChild};
 use std::{
     fs::File,
     io::{BufReader, Read},
+    sync::{Arc, RwLock},
+    thread,
 };
 
 const sep: &str = "@#@";
@@ -17,53 +19,61 @@ fn walk_run_children(run_children: &mut Vec<RunChild>) {
         }
     }
 }
-#[derive(Default)]
-struct DocXReader(Docx);
-
+struct DocXReader {
+    doc: Docx,
+    chunk_size: usize,
+    concurrent_request_size: usize,
+}
 impl DocXReader {
-    pub fn from_reader<T: Read>(self, reader: T) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn from_reader<T: Read>(reader: T) -> Result<Self, Box<dyn std::error::Error>> {
         let mut buffer = Vec::new();
         let _ = BufReader::new(reader).read_to_end(&mut buffer);
         let doc = docx_rs::read_docx(&buffer)?;
-        Ok(DocXReader(doc))
+        Ok(DocXReader {
+            doc,
+            chunk_size: 10,
+            concurrent_request_size: 5,
+        })
     }
 
     pub fn start_walk(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        for child in &mut self.0.document.children {
-            match child {
-                DocumentChild::Paragraph(p) => {
-                    let paragraph = &mut **p;
-                    let paragraph_children = &mut paragraph.children;
-                    Self::walk_paragraph_children(paragraph_children)?;
-                }
-                _ => {}
-            }
-        }
-        Ok(())
-    }
+        let mut chunk: Vec<*mut String> = Vec::new();
+        let mut con_request_handler: Vec<Vec<*mut String>> =
+            Vec::with_capacity(self.concurrent_request_size);
 
-    fn walk_paragraph_children(
-        paragraph_children: &mut Vec<ParagraphChild>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        for child in paragraph_children {
-            match child {
-                ParagraphChild::Run(run_data) => Self::walk_run_children(&mut run_data.children)?,
-                _ => {}
-            }
-        }
-        Ok(())
-    }
+        for child in &mut self.doc.document.children {
+            let para = match child {
+                DocumentChild::Paragraph(p) => &mut **p,
+                _ => continue,
+            };
+            for pchild in &mut para.children {
+                let run = match pchild {
+                    ParagraphChild::Run(r) => &mut **r,
+                    _ => continue,
+                };
+                for rchild in &mut run.children {
+                    let txt = match rchild {
+                        RunChild::Text(t) => t,
+                        _ => continue,
+                    };
+                    if txt.text.is_empty() {
+                        continue;
+                    }
+                    println!("{:?}", txt.text);
+                    // we keep of adding until we hit the chunk size limit
+                    chunk.push(&mut txt.text as *mut String);
 
-    fn walk_run_children(
-        run_children: &mut Vec<RunChild>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        for child in run_children {
-            match child {
-                RunChild::Text(txt) => {
-                    println!("{:?}", txt);
+                    // if we hit the limit
+                    // its the time to combine the chunks and make it as string with a sep, run a thread able to call tranlsation
+                    if chunk.len() >= self.chunk_size {
+                        // we push this to our request_handler
+                        con_request_handler.push(chunk);
+                        // and we reset the chunk for next iteration
+                        chunk = Vec::new();
+                    }
                 }
-                _ => {}
             }
+            // last chunk logic
         }
 
         Ok(())
@@ -72,7 +82,7 @@ impl DocXReader {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("/home/bedgirb/Downloads/test.docx")?;
-    let mut docx_reader = DocXReader::default().from_reader(file)?;
+    let mut docx_reader = DocXReader::from_reader(file)?;
     docx_reader.start_walk()?;
     /*
     let file = File::create("output.docx")?;
